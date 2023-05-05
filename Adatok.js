8484111018124ca4202686ab43f3cd65021be76d
Gergo <tar.gergo21@gmail.com>
2023-05-05 10:21:38 +0200

    json

--- Adatok.js
+++
@@ -1,7 +0,0 @@
-export const Adatok = [
-  { termek: "kéz", kor: 4, szin: "zöld" ,id:1},
-  { termek: "szem", kor: 9, szin: "kék" ,id:2},
-  { termek: "baba", kor: 100, szin: "piros" ,id:3},
-];
-
-export default Adatok;
--- Termekek.js
+++ Termekek.js
@@ -1,17 +1,22 @@
-import Adatok from "./adatok.js";
 import Termek from "./Termek.js";
+import Aszinkron from "./Aszinkron.js";
 class Termekek {
   #kedvencek = [];
   constructor() {
-    console.log(Adatok);
-    const szuloElem = $("article");
-    for (let index = 0; index < Adatok.length; index++) {
-      const elem = new Termek(Adatok[index], szuloElem);
-    }
+    const aszinkron = new Aszinkron();
+    let vegpont = "adatok.json";
+    aszinkron.adatBeolvas(vegpont, this.#termekMegjelenito);
     $(window).on("elemKattintas", (event) => {
       this.#kedvencek.push(event.detail);
       console.log(this.#kedvencek);
     });
+  }
+  #termekMegjelenito(Adatok) {
+    let sajatlista = Adatok.Adatok;
+    const szuloElem = $("article");
+    for (let index = 0; index < sajatlista.length; index++) {
+      const elem = new Termek(sajatlista[index], szuloElem);
+    }
   }
 }
 
